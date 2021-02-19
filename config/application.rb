5faef150a12a44925c919657b60d8c9b535ad2cc
Thomas Pietschmann <thomas@pietschi.com>
2021-02-19 21:29:33 +0100

    add turbolinks_render gem

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -15,7 +15,7 @@ module RestaurantsAjaxified
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    config.action_view.embed_authenticity_token_in_remote_forms = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
